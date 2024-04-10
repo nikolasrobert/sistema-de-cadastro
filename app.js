7bc463f56a981a96962400f53da84811bf795e23
nikolasrobert <nikolasfrobert@hotmail.com>
2024-04-10 12:29:44 -0300

    voltando para o estado inicial

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicacao esta se conectando ao banco de dados")
-// mais um console de teste
-console.log("O colaborador fez um console sem você ver")
-console.log("Enviando para o github")
