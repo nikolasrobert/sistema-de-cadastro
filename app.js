4e8cd3b422b1ff22f90df481d7929a56af3c6c27
nikolasrobert <nikolasfrobert@hotmail.com>
2024-04-13 13:34:25 -0300

    lo

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicacao esta se conectando ao banco de dados")
+// mais um console de teste
+console.log("O colaborador fez um console sem você ver")
+console.log("Enviando para o github")
